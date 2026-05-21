f39629f0415d8b46e59d03986fdc933d0381fbbf
Enrique Diaz <diazenrive@gmail.com>
2026-05-20 20:50:07 -0400

    network

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,24 +1,34 @@
 resource "aws_security_group" "app_sg" {
-  name = "legacy-users-sg"
+  name        = "legacy-users-sg"
+  description = "Security group for legacy users app"
 
   ingress {
-    from_port   = var.app_port
-    to_port     = var.app_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+
+    cidr_blocks = ["190.104.20.154/32"]
+  }
+
+  ingress {
+    description = "Application"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+
+    cidr_blocks = ["190.104.20.154/32"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "legacy-users-sg"
   }
 }
